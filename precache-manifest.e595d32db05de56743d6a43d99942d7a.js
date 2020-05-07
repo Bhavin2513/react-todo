5f7831c633263f405cd85a52369e52e9c91a1876
Bhavin2513 <bhavinpatel225.bp@gmail.com>
2020-05-07 20:39:38 +0530

    Updates

---
+++ precache-manifest.e595d32db05de56743d6a43d99942d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc8ed78f743e8387a5986dff62807ee",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "70de4af745b1b0a12556",
+    "url": "/react-todo/static/css/main.d540cd92.chunk.css"
+  },
+  {
+    "revision": "2987e9a823f3ba2edc53",
+    "url": "/react-todo/static/js/2.a783e81c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.a783e81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70de4af745b1b0a12556",
+    "url": "/react-todo/static/js/main.ff373cba.chunk.js"
+  },
+  {
+    "revision": "d24d4454d8cec70ed703",
+    "url": "/react-todo/static/js/runtime-main.848aa1ef.js"
+  }
+]);
